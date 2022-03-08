282363101fa55a4939575ec0c2a6a6de4a3fe565
Cory Pilat <56729741+cpilat97@users.noreply.github.com>
2022-03-08 07:42:37 -0500

    initial file upload and script to split pdfs

---
+++ R/split_pdf_and_rename_files.R
@@ -0,0 +1,39 @@
+library(here)
+library(pdftools)
+library(tidyverse)
+
+#I'm only going to use the first one hundred pages for this so i don't have to clean up 2000 pdfs, 
+#but other than the subset, the process should remain the same. 
+pdf_subset(input = 'files/tmp_fee_FEE _Notice_2021_1001_to_2000.pdf', pages = 1:100 , output = 'files/fees_100.pdf')
+
+
+#Read in the pdf text. This'll read in the text of each page. All the text is stored in a vector that 
+#should be the same length as the file numbers you're reading in. 
+pdf_page_text = pdf_text(pdf = 'files/fees_100.pdf')
+
+length(pdf_metadata)  #quick check
+
+#get case numbers from the pdf text for later renaming: 
+#The regex in the str extract is looking for numbers immediately proceeding text that says
+#"Case #: " or "de caso: " 
+case_numbers = str_extract(pdf_page_text, '(?<=Case #:\\s)\\d+|(?<=de caso:\\s)\\d+')
+
+#alternatively, if you want to keep the "case #: " or the "de caso" so you know if its the spanish file, 
+#we can do (also just going to do a bit of cleaning for them): 
+case_nums_alt = str_extract(pdf_page_text, 'Case #:\\s\\d+|de caso:\\s\\d+') %>% 
+  str_replace_all(., "\\s#:\\s|(?<=caso):\\s", "_num_") %>% 
+  str_replace_all(., "\\s", "_") %>% 
+  str_to_lower(.) %>% 
+  str_c('files/split_files/', ., '.pdf')
+
+#now to split the pdf into individual pages, read them in as a list, and then rename them
+#I'm going to put them into their own folder to make things easier to work with a bit later on: 
+split_files = pdf_split(input = 'files/fees_100.pdf', output = 'files/split_files/')
+
+#now time to rename them by iterating over the files. 
+renamed_files = map2(.x = split_files, 
+                     .y = case_nums_alt, 
+                     ~ file.rename(from = .x, 
+                                   to = .y))
+
+
--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # pdf_into_single_files
+
+Using the `pdftools` package to split large pdfs into individual pages. 
+
+Needs: 
+ - pdftools
+ - tidyverse (or stringr and purrr if you want indv. files)
+ 
+Optional: 
+  - here (which builds relative file paths to your R project so you don't need to use `setwd()`)
+  
+R script is in the R folder. 
+
+Feel free to modify the paths as you need. I only used a subset of 100 pages for this. 
+
